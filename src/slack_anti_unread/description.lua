f54f0eb04a57038cbb415a44567a0d150b395cea
Andrei Nekrasov <1828123+yuyoyuppe@users.noreply.github.com>
2026-06-16 13:26:57 +0200

    hide console window by default

--- src/slack_anti_unread/description.lua
+++ src/slack_anti_unread/description.lua
@@ -1,3 +1,3 @@
-kind "ConsoleApp"
-links {"utils"}
-dependson {"utils"}
+kind "WindowedApp"
+links {"model_runtime", "camera_capture", "gesture_engine", "renderer"}
+dependson {"model_runtime", "camera_capture", "gesture_engine", "renderer"}
